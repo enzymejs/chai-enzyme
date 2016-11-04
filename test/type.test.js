94ac0d6fff54fe6f6745d681a47c2885dbcfc876
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-11-04 13:52:54 +0100

    Add mount support

--- test/type.test.js
+++ test/type.test.js
@@ -1,3 +1,5 @@
+import {mount, shallow} from 'enzyme'
+
 class Foo extends React.Component {
   render () {
     return (
@@ -11,6 +13,12 @@ class Bar extends React.Component {
     return (
       <div>Bar</div>
     )
+  }
+}
+
+class Null extends React.Component {
+  render () {
+    return null
   }
 }
 
@@ -28,11 +36,24 @@ describe('#type', () => {
   describe('(type)', () => {
     it('passes when the actual matches the expected', (wrapper) => {
       expect(wrapper).to.have.type(Foo)
+      expect(wrapper.find(Foo)).to.have.type(Foo)
+      expect(shallow(<Null />)).to.have.type(null)
+      expect(shallow(<Bar />)).to.have.type('div')
+      expect(shallow(<div />)).to.have.type('div')
     }, { render: false, mount: false })
+
+    it('passes when the actual matches the expected', (wrapper) => {
+      expect(wrapper).to.have.type(Fixture)
+      expect(wrapper.find(Foo)).to.have.type(Foo)
+      expect(mount(<Null />)).to.have.type(Null)
+      expect(mount(<Bar />)).to.have.type(Bar)
+      expect(mount(<div />)).to.have.type('div')
+    }, { render: false, shallow: false })
 
     it('passes negated when the actual does not match the expected', (wrapper) => {
       expect(wrapper).to.not.have.type(Bar)
-    }, { render: false, mount: false })
+      expect(wrapper.find(Foo)).to.not.have.type(Bar)
+    }, { render: false })
 
     it('fails when the actual does not match the expected', (wrapper) => {
       expect(() => {
@@ -40,10 +61,16 @@ describe('#type', () => {
       }).to.throw('to be of type [Function: Bar], but it is of type [Function: Foo]')
     }, { render: false, mount: false })
 
+    it('fails when the actual does not match the expected', (wrapper) => {
+      expect(() => {
+        expect(wrapper).to.have.type(Bar)
+      }).to.throw('to be of type [Function: Bar], but it is of type [Function: Fixture]')
+    }, { render: false, shallow: false })
+
     it('fails when the actual is undefined', () => {
       expect(() => {
         expect(undefined).to.have.type(Foo)
       }).to.throw()
-    }, { render: false, mount: false })
+    }, { render: false })
   })
 })
