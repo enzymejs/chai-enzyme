f3563a4b031ea21eacb03ab16f2f9b035b13cb44
Marco de Jongh <m.jongh88@gmail.com>
2017-01-10 16:17:55 +1100

    Change exactlyCount check to type check fixes #141
    Without the type check ```expect(wrapper).to.have.exactly(0).descendants(selector);``` will not result as expected

--- src/assertions/descendants.js
+++ src/assertions/descendants.js
@@ -1,7 +1,7 @@
 export default function descendants ({ wrapper, markup, arg1, sig, flag }) {
   const exactlyCount = flag(this, 'exactlyCount')
 
-  if (exactlyCount) {
+  if (typeof exactlyCount === 'number') {
     const descendantCount = wrapper.getDescendantsCount(arg1)
 
     this.assert(
